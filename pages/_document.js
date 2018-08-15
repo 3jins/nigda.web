4d8879f460fdeedd764d5c0397c0a75ca51e0a3e
Sejin Jeon <jinaidy93@gmail.com>
2018-08-15 18:29:09 +0900

    Move a store from _app.js to _document.js

---
+++ pages/_document.js
@@ -0,0 +1,31 @@
+import Document, { Head, Main, NextScript } from 'next/document';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import flush from 'styled-jsx/server';
+
+import reducers from '../reducers/index';
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    const styles = flush();
+    return { ...initialProps, styles };
+  }
+
+  render() {
+    const store = createStore(reducers);
+    return (
+      <html lang="ko">
+        <Head>
+          <title>[니]가 [그]리고 싶은 거 [다] 그려!</title>
+        </Head>
+        <body>
+          <Provider store={store}>
+            <Main />
+          </Provider>
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
